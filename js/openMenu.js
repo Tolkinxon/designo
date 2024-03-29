882eb41f03c9440ecf6b8173c88fd0a5c3bb9161
Tolkinxon <krosstony639@gmail.com>
2024-03-29 08:18:42 +0300

    designs side finished

--- js/openMenu.js
+++ js/openMenu.js
@@ -1,6 +1,6 @@
 window.addEventListener('DOMContentLoaded', () => {
     const button = document.querySelector('.header__button')
-    const header = document.querySelector('.header')
+    const header = document.querySelector('body')
 
     button.addEventListener('click', () => {
         header.classList.toggle('open-menu')
